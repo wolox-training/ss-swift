f91e392d6613c2427ed1d962e637a9ec38989f91
sebastian <sebastian.spadea@wolox.com.ar>
2022-03-03 09:22:52 -0300

    starting with the library view

---
+++ WBooks/LibraryViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LibraryViewController.swift
+//  WBooks
+//
+//  Created by sebastian.a.spadea on 02/03/2022.
+//
+
+import UIKit
+
+class LibraryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
