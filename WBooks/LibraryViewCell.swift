69655208867a91e09040579f6435eb6bd415487e
sebastian <sebastian.spadea@wolox.com.ar>
2022-03-04 17:04:05 -0300

    hello darkness my old friend

--- WBooks/LibraryViewCell.swift
+++ WBooks/LibraryViewCell.swift
@@ -13,6 +13,17 @@ class LibraryViewCell: UITableViewCell {
     @IBOutlet var bookTitle: UILabel!
     @IBOutlet var bookCover: UIImageView!
     @IBOutlet var bookAuthor: UILabel!
+    @IBOutlet var cellView: UIView! {
+        didSet {
+            cellView.backgroundColor = .white
+            cellView.layer.cornerRadius = 5
+        }
+    }
+    
+    override func layoutSubviews() {
+        let contentInset = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
+        cellView.frame = cellView.frame.inset(by: contentInset)
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
--- WBooks/LibraryViewController.swift
+++ WBooks/LibraryViewController.swift
@@ -7,30 +7,34 @@
 
 import UIKit
 
-class LibraryViewController: UITableViewController {
+class LibraryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tableView: UITableView!
+    
+    var titles: [String] = []
+    var authors: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        let nib = UINib(nibName: "LibraryViewCell", bundle: nil)
-        tableView.register(nib, forCellReuseIdentifier: "LibraryViewCell")
+        tableView.register(LibraryViewCell.self, forCellReuseIdentifier: "LibraryViewCell")
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        tableView.backgroundColor = .lightGray
+        titles = ["A Little Bird Told Me","When the Doves Disappeared","The Best Book in the World","Be Creative","This is also UX"]
+        authors = ["Timothy Cross","Sofi Oksanen","Peter Sjernstrom","Tony Alcazar","Liliana Castilla"]
     }
 
     // MARK: - Table view data source
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 90
-    }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // #warning Incomplete implementation, return the number of rows
         let cell = tableView.dequeueReusableCell(withIdentifier: "LibraryViewCell", for: indexPath) as! LibraryViewCell
+        cell.bookTitle.text = titles[indexPath.row]
+        cell.bookAuthor.text = authors[indexPath.row]
+        cell.bookCover.image = UIImage(named: "img_book"+(String(indexPath.row+1)))
         return cell
     }
     
