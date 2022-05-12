53cb050264c49bad8ee9f2806dc0b7caa0465cb4
Sebastian Spadea <sebastian.a.spadea@accenture.com>
2022-05-12 17:12:45 -0300

    user default working

--- WBooks/Login/LoginViewController.swift
+++ WBooks/Login/LoginViewController.swift
@@ -10,19 +10,33 @@ import WolmoCore
 
 final class LoginViewController: UIViewController {
     private let loginView = LoginView()
-
+    private let loginViewModel: LoginViewModelProtocol
+    
+    init(loginViewModel: LoginViewModel) {
+        self.loginViewModel = loginViewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func loadView() {
         view = loginView
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if loginViewModel.readUserDefault() {
+            logInButton()
+        }
         loginView.onLogIn = logInButton
     }
 
     func logInButton() {
         let tabBar = TabBarController()
         tabBar.modalPresentationStyle = .fullScreen
+        loginViewModel.writeUserDefault()
         present(tabBar, animated: true)
     }
 }
--- WBooks/SceneDelegate.swift
+++ WBooks/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let window = UIWindow(windowScene: windowScene)
         
         /// 3. Create a view hierarchy programmatically
-        let viewController = LoginViewController()
+        let viewController = LoginViewController(loginViewModel: LoginViewModel())
         let navigation = UINavigationController(rootViewController: viewController)
         
         /// 4. Set the root view controller of the window with your view controller
